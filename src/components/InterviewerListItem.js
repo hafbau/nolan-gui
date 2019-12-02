8d4178ac71ef2de5caad30a82d3f795dee62541b
nolan <nolan796@live.ca>
2019-12-01 22:44:26 -0700

    InterviewerListitems

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import 'components/InterviewerListItem.scss';
+
+export default function InterviewerListItem(props) {
+  const { avatar, name, selected, setInterviewer } = props;
+
+  return (
+    <li
+      className={`interviewers__item ${
+        selected ? 'interviewers__item--selected' : ''
+      }`}
+      onClick={setInterviewer}
+    >
+      <img className="interviewers__item-image" src={avatar} alt={name} />
+      {selected ? name : ''}
+    </li>
+  );
+}
+
